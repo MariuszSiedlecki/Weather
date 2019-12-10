953fd102f234d7a8d87bcd464b5b05b9b5f07681
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-12-10 13:38:53 +0100

    Main

---
+++ src/main/java/marioWeather/Starter.java
@@ -0,0 +1,9 @@
+package marioWeather;
+
+import controllers.MainController;
+
+public class Starter {
+    public static void main(String[] args) {
+        new MainController().startApp();
+    }
+}
