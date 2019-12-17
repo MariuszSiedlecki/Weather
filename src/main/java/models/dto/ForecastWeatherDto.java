0336b52e967f4f204bee66e735b8747d58e16a2f
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-12-17 15:57:01 +0100

    extracting cloudy

---
+++ src/main/java/models/dto/ForecastWeatherDto.java
@@ -0,0 +1,14 @@
+package models.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ForecastWeatherDto {
+    @SerializedName("list")
+    private List<WeatherDto> forecastWeatherList;
+
+    public List<WeatherDto> getForecastWeatherList() {
+        return forecastWeatherList;
+    }
+}
